b4d0d9e41493f5b74d701a1051c64c6cf071e612
Felix Stegmaier <stegmaier.felix@gmail.com>
2019-06-14 22:28:29 +0200

    Simplify audio code

--- rust-cli/src/audio.rs
+++ rust-cli/src/audio.rs
@@ -25,69 +25,70 @@ fn format_supported(supported: &cpal::SupportedFormat, actual: &cpal::Format) ->
     supported.min_sample_rate <= actual.sample_rate
 }
 
-pub fn run() -> Result<(), Error> {
-    let mut reader = WavReader::open("res/samples/clap.wav")?;
-
-    let input_format = reader.spec();
-    let output_format = cpal::Format {
-        channels: input_format.channels,
-        sample_rate: cpal::SampleRate(input_format.sample_rate),
-        data_type: match input_format.sample_format {
+fn try_spec_to_format(spec: &hound::WavSpec) -> Result<cpal::Format, AudioError> {
+    Ok(cpal::Format {
+        channels: spec.channels,
+        sample_rate: cpal::SampleRate(spec.sample_rate),
+        data_type: match spec.sample_format {
             hound::SampleFormat::Float
-                if input_format.bits_per_sample == 32 => cpal::SampleFormat::F32,
+                if spec.bits_per_sample == 32 => cpal::SampleFormat::F32,
             hound::SampleFormat::Int
-                if input_format.bits_per_sample == 16 => cpal::SampleFormat::I16,
+                if spec.bits_per_sample == 16 => cpal::SampleFormat::I16,
             _ => Err(AudioError::InputFormatNotSupported)?,
         }
-    };
+    })
+}
+
+fn find_supported_format(device: &cpal::Device, format: &cpal::Format)
+    -> Result<cpal::SupportedFormat, Error>
+{
+    Ok(device.supported_output_formats()?
+        .find(|f| format_supported(&f, &format))
+        .ok_or(AudioError::OutputNotSupported)?)
+}
+
+struct Sampler<R: std::io::Read>(WavReader<R>);
+
+impl<R: std::io::Read> Sampler<R> {
+    fn sample<S: hound::Sample + Copy>(&mut self, fallback: S) -> S {
+        self.0.samples::<S>().next()
+            .map(|r| r.unwrap_or(fallback))
+            .unwrap_or(fallback)
+    }
+}
+
+pub fn run() -> Result<(), Error> {
+    let reader = WavReader::open("res/samples/clap.wav")?;
+
+    let input_format = reader.spec();
+    let output_format = try_spec_to_format(&input_format)?;
     println!("Using wav input format {:#?}", &input_format);
     println!("Trying to use device output format {:#?}", &output_format);
 
     let device = cpal::default_output_device()
         .ok_or(AudioError::NoOutputDevice)?;
     // check if there exists a format that supports the  wav_output_format
-    let mut formats = device.supported_output_formats()?;
-    let device_format = formats.find(|f| format_supported(&f, &output_format))
-        .ok_or(AudioError::OutputNotSupported)?;
-    print!("Matching device ouput format {:#?}", &device_format);
+    let device_format = find_supported_format(&device, &output_format)?;
+    println!("Matching device ouput format {:#?}", &device_format);
 
-    let format = output_format;
     let event_loop = cpal::EventLoop::new();
-    let stream_id = event_loop.build_output_stream(&device, &format)?;
+    let stream_id = event_loop.build_output_stream(&device, &output_format)?;
     event_loop.play_stream(stream_id.clone());
 
-    // let sample_rate = format.sample_rate.0 as f32;
-    // let mut sample_clock = 0f32;
-
-    // let tone = Arc::new(Mutex::new(440.0_f32));
-    // let tone2 = tone.clone();
-
-
-
-    // // Produce a sinusoid of maximum amplitude.
-    // let mut next_value = move || {
-    //     // sample_clock = (sample_clock + 1.0) % sample_rate;
-    //     // (sample_clock * (*tone.deref().lock().unwrap()) * 2.0 * PI / sample_rate).sin()
-    // };
+    let mut sampler = Sampler(reader);
 
     let t1 = thread::spawn(move || {
         event_loop.run(move |_, data| {
             match data {
                 // format has been determined beforehand, so using the samples with the correct type
                 cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::I16(mut buffer) } => {
-                    for output_sample in buffer.chunks_mut(format.channels as usize) {
-                        let mut input_samples = reader.samples::<i16>().map(|s| s.unwrap()).chain(iter::repeat(0));
-                        for out in output_sample.iter_mut() {
-                            *out = input_samples.next().unwrap() // samples are interleaved
-                        }
+                    for out in buffer.iter_mut(){
+                        *out = sampler.sample::<i16>(0) // samples are interleaved per channel
                     }
                 },
                 cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
-                    for output_sample in buffer.chunks_mut(format.channels as usize) {
-                        let mut input_samples = reader.samples::<f32>().map(|s| s.unwrap()).chain(iter::repeat(0.0));
-                        for out in output_sample.iter_mut() {
-                            *out = input_samples.next().unwrap(); // samples are interleaved
-                        }
+                    for out in buffer.iter_mut() {
+                        *out = sampler.sample::<f32>(0.0) // samples are interleaved per channel
                     }
                 },
                 _ => (),
@@ -95,16 +96,6 @@ pub fn run() -> Result<(), Error> {
         });
     });
 
-    // let t2 = thread::spawn(move || {
-    //     loop {
-    //         *tone2.deref().lock().unwrap() = 4400.0_f32;
-    //         thread::sleep(Duration::from_millis(500));
-    //         *tone2.deref().lock().unwrap() = 880.0_f32;
-    //         thread::sleep(Duration::from_millis(500));
-    //     }
-    // });
-
     t1.join().unwrap();
-    // t2.join().unwrap();
     Ok(())
 }

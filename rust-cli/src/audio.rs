04bafbf4a725cfbdd4bf119228daaf9020f4813b
Felix Stegmaier <stegmaier.felix@gmail.com>
2019-06-15 17:41:21 +0200

    Add MultiSampler

--- rust-cli/src/audio.rs
+++ rust-cli/src/audio.rs
@@ -1,12 +1,7 @@
 use cpal;
 use failure::{Error, Fail};
 use hound::WavReader;
-use std::f32::consts::PI;
-use std::sync::{Arc, Mutex};
-use std::ops::Deref;
-use std::thread;
-use std::time::Duration;
-use std::iter;
+use std::{io, thread, sync::{Arc, Mutex}, time::Duration};
 
 #[derive(Fail, Debug)]
 enum AudioError {
@@ -16,6 +11,8 @@ enum AudioError {
     InputFormatNotSupported,
     #[fail(display = "WAV sample format not supported by output device")]
     OutputNotSupported,
+    #[fail(display = "WAV sample format must be the same for all samples")]
+    FormatMismatch,
 }
 
 fn format_supported(supported: &cpal::SupportedFormat, actual: &cpal::Format) -> bool {
@@ -47,18 +44,76 @@ fn find_supported_format(device: &cpal::Device, format: &cpal::Format)
         .ok_or(AudioError::OutputNotSupported)?)
 }
 
-struct Sampler<R: std::io::Read>(WavReader<R>);
+trait Sample: hound::Sample + Copy {
+    fn zero() -> Self;
+    fn saturating_add_sample(self, other: Self) -> Self;
+}
 
-impl<R: std::io::Read> Sampler<R> {
-    fn sample<S: hound::Sample + Copy>(&mut self, fallback: S) -> S {
+impl Sample for f32 {
+    fn zero() -> Self { 0.0 }
+    fn saturating_add_sample(self, other: Self) -> Self {
+        let r = self + other;
+        if r < -1.0 { return -1.0; }
+        if r > 1.0 { return 1.0; }
+        return r;
+    }
+}
+
+impl Sample for i16 {
+    fn zero() -> Self { 0 }
+    fn saturating_add_sample(self, other: Self) -> Self {
+        self.saturating_add(other)
+    }
+}
+
+struct Sampler<R: io::Read>(WavReader<R>);
+
+impl<R: io::Read> Sampler<R> {
+    // if None is returned the reader has ended or failed
+    // samples are interleaved per channel
+    fn sample<S: Sample>(&mut self) -> Option<S> {
         self.0.samples::<S>().next()
-            .map(|r| r.unwrap_or(fallback))
-            .unwrap_or(fallback)
+            .and_then(|r| r.ok())
+    }
+}
+
+struct MultiSampler<R: io::Read> {
+    samplers: Vec<Sampler<R>>,
+    common_format: cpal::Format,
+}
+
+
+impl<R: io::Read> MultiSampler<R> {
+    fn new(format: cpal::Format) -> Self {
+        MultiSampler {
+            samplers: Vec::new(),
+            common_format: format,
+        }
+    }
+    fn add_reader(&mut self, reader: WavReader<R>) -> Result<(), Error> {
+        let format = try_spec_to_format(&reader.spec())?;
+        println!("Trying to add reader with format {:#?}", &format);
+        if self.common_format != format {
+            Err(AudioError::FormatMismatch)?;
+        }
+        let sampler = Sampler(reader);
+        self.samplers.push(sampler);
+        Ok(())
+    }
+    fn sample<S: Sample>(&mut self) -> S {
+        let mut sum = S::zero();
+        for sampler in self.samplers.iter_mut() {
+            if let Some(s) = sampler.sample::<S>() {
+                sum = sum.saturating_add_sample(s)
+            }
+            // FIXME: remove sampler from vec otherwise, as its done now
+        }
+        sum
     }
 }
 
 pub fn run() -> Result<(), Error> {
-    let reader = WavReader::open("res/samples/clap.wav")?;
+    let reader = WavReader::open("res/samples/kick.wav")?;
 
     let input_format = reader.spec();
     let output_format = try_spec_to_format(&input_format)?;
@@ -75,7 +130,9 @@ pub fn run() -> Result<(), Error> {
     let stream_id = event_loop.build_output_stream(&device, &output_format)?;
     event_loop.play_stream(stream_id.clone());
 
-    let mut sampler = Sampler(reader);
+    let sampler = MultiSampler::new(output_format);
+    let shared_sampler_1 = Arc::new(Mutex::new(sampler));
+    let shared_sampler_2 = shared_sampler_1.clone();
 
     let t1 = thread::spawn(move || {
         event_loop.run(move |_, data| {
@@ -83,12 +140,14 @@ pub fn run() -> Result<(), Error> {
                 // format has been determined beforehand, so using the samples with the correct type
                 cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::I16(mut buffer) } => {
                     for out in buffer.iter_mut(){
-                        *out = sampler.sample::<i16>(0) // samples are interleaved per channel
+                        // FIXME: unwrap
+                        *out = shared_sampler_1.lock().unwrap().sample::<i16>() // samples are interleaved per channel
                     }
                 },
                 cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
                     for out in buffer.iter_mut() {
-                        *out = sampler.sample::<f32>(0.0) // samples are interleaved per channel
+                        // FIXME: unwrap
+                        *out = shared_sampler_1.lock().unwrap().sample::<f32>() // samples are interleaved per channel
                     }
                 },
                 _ => (),
@@ -96,6 +155,26 @@ pub fn run() -> Result<(), Error> {
         });
     });
 
+    let t2 = thread::spawn(move || {
+        loop {
+            shared_sampler_2.lock().unwrap()
+                .add_reader(WavReader::open("res/samples/kick.wav").unwrap()).unwrap();
+            thread::sleep(Duration::from_millis(250));
+            shared_sampler_2.lock().unwrap()
+                .add_reader(WavReader::open("res/samples/kick.wav").unwrap()).unwrap();
+            thread::sleep(Duration::from_millis(250));
+            shared_sampler_2.lock().unwrap()
+                .add_reader(WavReader::open("res/samples/kick.wav").unwrap()).unwrap();
+            thread::sleep(Duration::from_millis(250));
+            shared_sampler_2.lock().unwrap()
+                .add_reader(WavReader::open("res/samples/kick.wav").unwrap()).unwrap();
+            shared_sampler_2.lock().unwrap()
+                .add_reader(WavReader::open("res/samples/cowbell.wav").unwrap()).unwrap();
+            thread::sleep(Duration::from_millis(250));
+        }
+    });
+
     t1.join().unwrap();
+    t2.join().unwrap();
     Ok(())
 }
